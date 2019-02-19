c4178aef027215a8cd7ba33273b5687dfeb239ca
Xavier Chua <xavierc.1998@gmail.com>
2019-02-19 14:45:08 -0800

    Updated Enemy Wandering Lerp

--- FluSeason/Assets/Scripts/EnemyBehaviour.cs
+++ FluSeason/Assets/Scripts/EnemyBehaviour.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class EnemyBehaviour : MonoBehaviour
 {
 
-    private Vector3 origin;
-    private Vector3 wanderingTo;
+    private Vector3 origin = new Vector3(0,0,0);
+    private Vector3 wanderingTo = new Vector3(0,0,0);
     private float wanderTime;
+    private bool isWandering = false;
 
     public float wanderDistance = 5f;
-    public float wanderRate = 15f;
-    public float travelSpeed = 0.5f;
+    public float wanderRate = 10f;
+    //public float travelSpeed = 0.5f;
 
     // Start is called before the first frame update
     private void Awake()
@@ -31,13 +32,32 @@ public class EnemyBehaviour : MonoBehaviour
         {
             Debug.Log("wanderTime updated");
             wanderTime = Time.time;
-            Wander();
+            if (!isWandering)
+            {
+                SetWander();
+            }
+
         }
+        Wander();
     }
 
     void Wander()
-    {
-        wanderingTo = origin + (Vector3)(Random.insideUnitCircle * wanderDistance);
-        transform.position = Vector3.Lerp(this.transform.position, wanderingTo, travelSpeed);
+    { 
+        //Debug.Log(wanderingTo);
+        transform.position = Vector3.Lerp(this.transform.position, wanderingTo, Time.deltaTime/2);
+        if(Vector3.Distance(this.transform.position, wanderingTo) <= 1.0f)
+        {
+            isWandering = false;
+        }
     }
+
+    void SetWander()
+    {
+        //wanderingTo = new Vector3(origin.x + (Random.Range(-1.0f, 1.0f) * wanderDistance),
+        //      origin.y + (Random.Range(-1.0f, 1.0f) * wanderDistance), 0);
+
+        wanderingTo = origin + (Vector3)(Random.insideUnitCircle * wanderDistance);
+        isWandering = true;
+    }
+
 }

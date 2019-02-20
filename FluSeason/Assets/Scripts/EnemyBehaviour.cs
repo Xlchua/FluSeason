d73cdf48ab980c0cbd03069f7a9f100552c45c58
Xavier Chua <xavierc.1998@gmail.com>
2019-02-19 22:08:32 -0800

    Added enemystates onto enemy behavior to future proof for attacking/following player

--- FluSeason/Assets/Scripts/EnemyBehaviour.cs
+++ FluSeason/Assets/Scripts/EnemyBehaviour.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum EnemyState {Idle, Patrolling, Following, Attacking};
+
 public class EnemyBehaviour : MonoBehaviour
 {
 
@@ -9,15 +11,20 @@ public class EnemyBehaviour : MonoBehaviour
     private Vector3 wanderingTo = new Vector3(0,0,0);
     private float wanderTime;
     private bool isWandering = false;
+    private EnemyState m_state = EnemyState.Patrolling;
 
-    public float wanderDistance = 5f;
-    public float wanderRate = 10f;
+    public float patrolDistance = 5f;
+    public float patrolRate = 10f;
+    public float attackRange;
+    public float aggroRange;
+
     //public float travelSpeed = 0.5f;
 
     // Start is called before the first frame update
     private void Awake()
     {
         origin = this.transform.position;
+        
     }
 
     void Start()
@@ -28,7 +35,43 @@ public class EnemyBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Time.time > wanderTime + wanderRate)
+        /* Player Not Implemented Yet.
+
+        float rangeFromPlayer = Vector3.Distance(player.transform.position, this.transform.position);
+
+        if(rangeFromPlayer =< attackRange)
+        {
+            m_state = EnemyState.Attacking;
+        }
+
+
+        if(rangeFromPlayer > attackRange && rangeFromPlayer <= aggroRange)
+        {
+            m_state = EnemyState.Following;
+        }
+        */
+
+        if (m_state != EnemyState.Patrolling)
+            m_state = EnemyState.Patrolling;
+
+        switch (m_state)
+        {
+            case EnemyState.Patrolling:
+                Wander();
+                break;
+            case EnemyState.Following:
+                break;
+            case EnemyState.Attacking:
+                break;
+            case EnemyState.Idle:
+                break;
+
+        }
+    }
+
+    void Wander()
+    {
+        if (Time.time > wanderTime + patrolRate)
         {
             Debug.Log("wanderTime updated");
             wanderTime = Time.time;
@@ -38,13 +81,8 @@ public class EnemyBehaviour : MonoBehaviour
             }
 
         }
-        Wander();
-    }
-
-    void Wander()
-    { 
         //Debug.Log(wanderingTo);
-        transform.position = Vector3.Lerp(this.transform.position, wanderingTo, Time.deltaTime/2);
+        transform.position = Vector3.Lerp(this.transform.position, wanderingTo, Time.deltaTime);
         if(Vector3.Distance(this.transform.position, wanderingTo) <= 1.0f)
         {
             isWandering = false;
@@ -53,10 +91,17 @@ public class EnemyBehaviour : MonoBehaviour
 
     void SetWander()
     {
-        //wanderingTo = new Vector3(origin.x + (Random.Range(-1.0f, 1.0f) * wanderDistance),
-        //      origin.y + (Random.Range(-1.0f, 1.0f) * wanderDistance), 0);
+        //wanderingTo = new Vector3(origin.x + (Random.Range(-1.0f, 1.0f) * patrolDistance),
+        //      origin.y + (Random.Range(-1.0f, 1.0f) * patrolDistance), 0);
 
-        wanderingTo = origin + (Vector3)(Random.insideUnitCircle * wanderDistance);
+        float r = patrolDistance * Mathf.Sqrt(Random.value);
+        float theta = Mathf.PI * Random.value * 2;
+
+        float randX = r * Mathf.Cos(theta);
+        float randY = r * Mathf.Sin(theta);
+
+        wanderingTo = origin + new Vector3(randX, randY);
+        //wanderingTo = this.transform.position + (Vector3)(Random.insideUnitCircle * patrolDistance);
         isWandering = true;
     }
 

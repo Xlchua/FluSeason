dd90ce1816c5f91b89f53952ce0e367b426583f2
Xavier Chua <xavierc.1998@gmail.com>
2019-02-24 15:06:27 -0800

    Changed player colliders to 2D, added rigidbody 2D
    updated player script to rotate wherever mouse is at.

--- FluSeason/Assets/Scripts/SimplePlayerMove.cs
+++ FluSeason/Assets/Scripts/SimplePlayerMove.cs
@@ -6,7 +6,14 @@ public class SimplePlayerMove : MonoBehaviour
 {
     public float moveSpeed = 8f;
 
+
+    private Rigidbody2D rb;
     Vector3 direction;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +26,24 @@ public class SimplePlayerMove : MonoBehaviour
     {
         direction = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
-        transform.Translate(direction * moveSpeed * Time.deltaTime);
+        Look();
+        //transform.Translate(direction * moveSpeed * Time.deltaTime);
     }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = direction * moveSpeed;
+    }
+
+
+    void Look()
+    {
+        Vector3 mousePos = Input.mousePosition;
+
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+        this.transform.rotation = Quaternion.Euler(new Vector3(0f,0f, angle));
+    }
+
+
 }

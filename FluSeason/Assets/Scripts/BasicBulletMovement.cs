3b51c809a928df5804b8a76e0e7d1fd38e598969
shishir-reddy <shishir1@uci.edu>
2019-02-24 21:03:18 -0800

    Added player shooting
    Shooting works by coroutine. When the player holds down left mouse a bullet is spawned every 0.2 seconds, and is fired towards the mouse position. Bullets self destruct after 2 seconds.

---
+++ FluSeason/Assets/Scripts/BasicBulletMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicBulletMovement : MonoBehaviour
+{
+    private float bulletSpeed = 20f;
+
+    private Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * bulletSpeed;
+    }
+
+    void Update()
+    {
+        Destroy(this.gameObject, 2);
+    }
+}
--- FluSeason/Assets/Scripts/SimplePlayerMove.cs
+++ FluSeason/Assets/Scripts/SimplePlayerMove.cs
@@ -6,13 +6,23 @@ public class SimplePlayerMove : MonoBehaviour
 {
     public float moveSpeed = 8f;
 
+    public float bulletSpawnInterval = 0.2f;
+
+    [SerializeField]
+    public GameObject BasicBullet;
+
+    private bool isBulletSpawning = false;
+
+    private IEnumerator fireCoroutine;
 
     private Rigidbody2D rb;
+
     Vector3 direction;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        fireCoroutine = FireCoroutine();
     }
 
     // Start is called before the first frame update
@@ -28,6 +38,16 @@ public class SimplePlayerMove : MonoBehaviour
 
         Look();
         //transform.Translate(direction * moveSpeed * Time.deltaTime);
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            StopShooting();
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if(!isBulletSpawning)
+                StartShooting();
+        }
+        
     }
 
     private void FixedUpdate()
@@ -38,11 +58,48 @@ public class SimplePlayerMove : MonoBehaviour
 
     void Look()
     {
-        Vector3 mousePos = Input.mousePosition;
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        float angle = Mathf.Atan2(mousePos.y - this.transform.position.y, mousePos.x - this.transform.position.x) * Mathf.Rad2Deg;
 
         this.transform.rotation = Quaternion.Euler(new Vector3(0f,0f, angle));
+
+        DrawLine(mousePos);
+    }
+
+    void DrawLine(Vector3 mousePos)
+    {
+        LineRenderer line = this.GetComponent<LineRenderer>();
+
+        mousePos.Scale(new Vector3(1, 1, 0));
+
+        Vector3[] endpoints = new Vector3[2] { new Vector3(this.transform.position.x, this.transform.position.y), mousePos };
+        line.startWidth = 0.4f;
+        line.endWidth = 0.4f;
+        line.SetPositions(endpoints);
+    }
+
+    void StartShooting()
+    {
+        StartCoroutine(fireCoroutine);
+        Debug.Log("Starting Shooting");
+        isBulletSpawning = true;
+    }
+
+    void StopShooting()
+    {
+        StopCoroutine(fireCoroutine);
+        Debug.Log("Stopping Shooting");
+        isBulletSpawning = false;
+    }
+
+    IEnumerator FireCoroutine()
+    {
+        while (true)
+        {
+            Instantiate(BasicBullet, this.transform.position, this.transform.rotation);
+            yield return new WaitForSeconds(bulletSpawnInterval);
+        }
     }
 
 

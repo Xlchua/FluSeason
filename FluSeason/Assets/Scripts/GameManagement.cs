c6f578a072315f0a0bde290c009f9ec2561c151c
shishir-reddy <shishir1@uci.edu>
2019-02-25 18:30:05 -0800

    Added a wave system, modified timer
    Added a game manger to keep track of waves and time. The timer and wave texts are both set through the game manager. The game manager also has an update wave function to handle what happens every time a new wave starts - we can handle difficulty increases and stuff in this method.

---
+++ FluSeason/Assets/Scripts/GameManagement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManagement : MonoBehaviour
+{
+    public float timeElapsed = 0f;
+
+    public int wave = 10000;
+
+    public static GameManagement instance;
+
+    public TextMeshProUGUI wave_meshProUGUI;
+
+    public TextMeshProUGUI timer_meshProUGUI;
+
+    private int waveAddition = 0;
+
+    private int waveIncrement = 10;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Every 10 seconds new wave. A little janky because timeElapsed is a float so regular modulus doesn't work
+        if (timeElapsed >= waveAddition)
+            UpdateWave();
+
+        timeElapsed += Time.deltaTime;
+        timer_meshProUGUI.text = string.Format("Timer: {0:0.00}", timeElapsed);
+
+    }
+
+    void UpdateWave()
+    {
+        wave += 1;
+        waveAddition += waveIncrement;
+        wave_meshProUGUI.text = string.Format("Wave: {0:0}", wave);
+        //Do things that happen after a new wave starts
+    }
+}

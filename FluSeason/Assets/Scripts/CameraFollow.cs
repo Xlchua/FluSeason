b5f1f0e6d44dfe34810a80f0bddc890b8529cc73
shishir-reddy <shishir1@uci.edu>
2019-03-03 21:39:56 -0800

    Camera tracking implemented
    Camera now follows the player through the background and stays within bounds.

---
+++ FluSeason/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+
+    public Vector3 offset;
+
+    public Vector2 minCameraPos;
+    public Vector2 maxCameraPos;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = player.transform.position + offset;
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, minCameraPos.x, maxCameraPos.x),
+                                         Mathf.Clamp(transform.position.y, minCameraPos.y, maxCameraPos.y),
+                                                     transform.position.z);
+    }
+}
